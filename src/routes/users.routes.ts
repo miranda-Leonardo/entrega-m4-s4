7673eec46dddc98381cccaa7cb270d903070327b
Leonardo Miranda do Nascimento <leonardo.miranda.3@hotmail.com>
2022-12-21 16:52:28 -0400

    add: list users route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { createUserController } from '../controllers/user.controller';
+import { createUserController, listUserController } from '../controllers/user.controller';
+import { ensureAuthMiddleware } from '../middleware/ensureAuth.middleware';
 import { ensureDataIsValidMiddleware } from '../middleware/ensureDataIsValid.middleware';
 import { userCreateSerializer } from '../serializers/user.serializers';
 
 const userRoutes = Router();
 
 userRoutes.post('', ensureDataIsValidMiddleware(userCreateSerializer), createUserController);
+userRoutes.get('', ensureAuthMiddleware, listUserController);
 
 export default userRoutes
