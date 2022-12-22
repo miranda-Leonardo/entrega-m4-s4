1e9118da7e9dab8b0d7a987c708906a35c4bdf97
Leonardo Miranda do Nascimento <leonardo.miranda.3@hotmail.com>
2022-12-22 16:22:30 -0400

    fix: verify path route

--- src/services/users/listUser.service.ts
+++ src/services/users/listUser.service.ts
@@ -1,12 +1,18 @@
 import AppDataSource from "../../data-source";
 import { User } from "../../entities/user.entity";
+import { IUserCreateResponse } from "../../interfaces/users";
+import { listUserSerializer } from "../../serializers/user.serializers";
 
-const listUserService = async (): Promise<User[]> => {
+const listUserService = async () => {
     const userRepository = AppDataSource.getRepository(User);
 
     const users = await userRepository.find();
+    
+    const listUsers = await listUserSerializer.validate(users, {
+        stripUnknown: true
+    });
 
-    return users;
+    return listUsers;
 };
 
 export { listUserService };
--- src/services/users/updateUser.service.ts
+++ src/services/users/updateUser.service.ts
@@ -1,14 +1,13 @@
 import AppDataSource from '../../data-source';
 import { User } from '../../entities/user.entity';
 import { AppError } from '../../errors/AppError';
-import { IUserUpdate, IUser } from '../../interfaces/users/index';
+import { IUserUpdate, IUserCreateResponse } from '../../interfaces/users/index';
 import { userResponseSerializer, userUpdateSerializer } from '../../serializers/user.serializers';
 
-const updateUserService = async ( userData: IUserUpdate, userId: string, userloggedInId: string ): Promise<IUser> => {
+const updateUserService = async ( userData: IUserUpdate, userId: string, userloggedInId: string ): Promise<IUserCreateResponse> => {
     if(userId.length !== 36) {
         throw new AppError('User not exists!', 404);
     };
-
     
     const userRepository = AppDataSource.getRepository(User);
     
@@ -16,7 +15,7 @@ const updateUserService = async ( userData: IUserUpdate, userId: string, userlog
     if( !userLoggedIn?.isAdm && (userloggedInId !== userId) ) {
         throw new AppError("You don't have admin permission!", 401);
     };
-    
+
     if( !Object.keys(userData).length ) {
         throw new AppError('You cannot update this data!', 401);
     };
@@ -26,15 +25,11 @@ const updateUserService = async ( userData: IUserUpdate, userId: string, userlog
     if(!user) {
         throw new AppError('User not exists!', 404);
     };
-    console.log({ user, userData});
-    const updateUser = userRepository.create({ ...user, ...userData });
-    await userRepository.save(updateUser);
 
-    const updateUserResponse = await userResponseSerializer.validate(updateUser, {
-        stripUnknown: true
-    });
+    const updateUser = await userRepository.update({ id: userId }, userData);
+    const userResponse = Object.assign(user, userData)
 
-    return updateUserResponse;
+    return userResponse;
 };
 
 export { updateUserService };
